5cca3bbfbf772eb4178e7ae4267855fd33296085
JayaKirthika <jayakirthikaanbhu@gmail.com>
2023-12-17 08:42:44 +0530

    Use products.js file

--- scripts/amazon.js
+++ scripts/amazon.js
@@ -3,31 +3,9 @@ MAIN IDEA OF JS:
    =>Save the data
    =>Generate the HTML
    =>Make it interactive 
-*/const products = [{   // creating everything as object
-    image: 'images/products/athletic-cotton-socks-6-pairs.jpg',
-    name: 'Black and Gray Athletic Cotton Socks - 6 Pairs',
-    rating: {
-        stars: 4.5,
-        count: 87
-    },
-    priceCents: 1090   // saving in cents instead dollar
-}, {
-    image: 'images/products/intermediate-composite-basketball.jpg',
-    name: 'Intermediate Size Basketball',
-    rating: {
-        stars: 4,
-        count: 127
-    },
-    priceCents: 2095
-}, {
-    image: 'images/products/adults-plain-cotton-tshirt-2-pack-teal.jpg',
-    name: 'Adults Plain Cotton T-Shirt - 2 Pack',
-    rating: {
-        stars: 4.5,
-        count: 56
-    },
-    priceCents: 799
-}];
+*/
+// creating everything as object and its done in another js file
+
 
 let productsHTML = '';
 
