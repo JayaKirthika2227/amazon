5f725b845d15f3ab0df45da1402aa642ac06d4a5
JayaKirthika <jayakirthikaanbhu@gmail.com>
2023-12-17 11:31:53 +0530

    Make add to cart button interactive

--- scripts/amazon.js
+++ scripts/amazon.js
@@ -6,31 +6,30 @@ MAIN IDEA OF JS:
 */
 // creating everything as object and its done in another js file
 
-
 let productsHTML = '';
 
-products.forEach((products) => {
+products.forEach((product) => {
     productsHTML = productsHTML + `
             <div class="product-container">
             <div class="product-image-container">
             <img class="product-image"
-                src="${products.image}">   
+                src="${product.image}">   
             </div>
 
             <div class="product-name limit-text-to-2-lines">
-            ${products.name}
+                ${product.name}
             </div>
 
             <div class="product-rating-container">
             <img class="product-rating-stars"
-                src="images/ratings/rating-${products.rating.stars * 10}.png">
+                src="images/ratings/rating-${product.rating.stars * 10}.png">
             <div class="product-rating-count link-primary">
-                ${products.rating.count}
+                ${product.rating.count}
             </div>
             </div>
 
             <div class="product-price">
-            ${(products.priceCents /100).toFixed(2)}
+            ${(product.priceCents /100).toFixed(2)}
             </div>
 
             <div class="product-quantity-container">
@@ -55,13 +54,39 @@ products.forEach((products) => {
             Added
             </div>
 
-            <button class="add-to-cart-button button-primary">
-            Add to Cart
+            <button class="add-to-cart-button button-primary 
+            js-add-to-cart" data-product-id="${product.id}">  
+                Add to Cart
             </button>
         </div>
     `;
 });
-
-console.log(productsHTML);
+                            //this data-product-name="${products.name}" is a data attribute
 
 document.querySelector('.js-products-grid').innerHTML = productsHTML;
+
+document.querySelectorAll('.js-add-to-cart').forEach((button) => {
+    button.addEventListener('click' , () => {
+        const productId = button.dataset.productId;   //dataset - gives all the data attributes attached to ${products.id} button
+                                            // in button.dataset.productId the productId is converted into camelCase from kebab-case
+        let matchingItem;
+        
+        cart.forEach((item) => {
+            if (productId === item.productId) {
+                matchingItem = item;
+            }
+        });
+        
+        if (matchingItem) {
+            matchingItem.quantity += 1;
+        } else {
+            cart.push({
+                productId: productId ,
+                quantity : 1
+            });
+        }
+
+        
+        console.log(cart);
+    });
+});
